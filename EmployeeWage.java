6309203244eb8f5765760869e7f9ba9b2b24fb19
omkar <omkardudhewar121@gmail.com>
2022-10-13 15:00:22 +0530

    daily wage of employee is added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,10 +1,15 @@
 package EmployeeWage;
 public class EmployeeWage {
+           static final int WAGE_PER_HOUR = 20;
+           static final int FULL_DAY_HOUR = 8;
+
     public static void main(String[] args) {
-            System.out.println("Welcome to Employee Wage Program");
-            double empCheck = Math.floor(Math.random()*10)%2;
+        int dailyEmployeeWage = 0;
+        double empCheck = Math.floor(Math.random()*10)%2;
             if (empCheck==1){
                 System.out.println("Employee is Present");
+                dailyEmployeeWage = WAGE_PER_HOUR*FULL_DAY_HOUR;
+                System.out.println("Daily wage of employee ="+ dailyEmployeeWage);
             }
             else {
                 System.out.println("Employee is Absent");
