4dcaa305aa0a588b1977678af360b892aa228087
omkar <omkardudhewar121@gmail.com>
2022-10-13 14:34:58 +0530

    attendence added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,14 +3,12 @@ package EmployeeWage;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Program");
-        int Is_FULL_TIME = 1;
         double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == Is_FULL_TIME) {
+        if (empCheck == 1) {
             System.out.println("Employee is Present");
         }
         else{
-                System.out.println("Employee is Absent");
-            }
+            System.out.println("Employee is Absent");
         }
     }
-
+}
