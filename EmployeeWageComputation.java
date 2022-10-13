3c87b1e8a97fee22ea23a877192fa2fc474d6df8
omkar <omkardudhewar121@gmail.com>
2022-10-13 15:22:15 +0530

    attendence added

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -3,5 +3,12 @@ package EmployeeWageComputation;
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+        double empCheck = (int)Math.floor(Math.random()*10)%2;
+        if (empCheck==1){
+            System.out.println("Employee is Present");
+        }
+        else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
